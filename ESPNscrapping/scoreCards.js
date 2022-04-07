6f1944ab05c88721c60ba8b30f8d4536b442fc46
Thakur-Vishal-Pratap <vishalpratap844@gmail.com>
2022-04-08 00:21:07 +0530

    ESPN scrapping almost complete

--- ESPNscrapping/scoreCards.js
+++ ESPNscrapping/scoreCards.js
@@ -21,13 +21,54 @@ function getMatchDetails(html){
     // console.log(desc.text());
     let descArr = desc.text().split(",");
     // console.log(descArr);
+
+    // get date of the Match
     let dateOfMAtch = descArr[2];
+
+    //get Venue of the Match
     let VenueOfMAtch = descArr[1];
 
+    //get result of the match
     let matchResEle = selecTool('p[class="ds-text-tight-m ds-font-regular ds-truncate ds-text-typo-title"]');        
     let matchResult = matchResEle.text();
 
-    
+
+    //get both team names
+    let teamNames = selecTool('a[class="ds-text-ui-typo hover:ds-text-ui-typo-primary ds-block"]');        
+    let Team1 = selecTool(teamNames[0]).text();
+    let Team2 = selecTool(teamNames[1]).text();
+    //console.log(Team1);
+    //console.log(Team2);
+
+    //get innings
+    let allBatsManTable = selecTool('table[class="ds-w-full ds-table ds-table-xs ds-table-fixed ci-scorecard-table"] tbody');
+    // console.log(allBatsManRows.text());
+    let htmlString = "";
+    for(let i=0; i<allBatsManTable.length ; i++){
+        htmlString += selecTool(allBatsManTable[i]).html();
+
+        let allRows = selecTool(allBatsManTable[i]).find("tr");
+
+        for(let i=0 ; i<allRows.length ; i++){
+            let row = selecTool(allRows[i]);
+            if(selecTool(row).hasClass("ds-text-tight-s")){
+                let firstColofRow = row.find("td")[0];
+                if(selecTool(firstColofRow).hasClass("ds-min-w-max")){
+                    let playerName = selecTool(row.find("td")[0]).text();
+                    let runs = selecTool(row.find("td")[2]).text();
+                    let balls = selecTool(row.find("td")[3]).text();
+                    let numOf4 = selecTool(row.find("td")[5]).text();
+                    let numOf6 = selecTool(row.find("td")[6]).text();
+                    let runRate = selecTool(row.find("td")[7]).text();
+
+                    console.log(
+                        `${playerName} | ${runs} | ${balls} | ${numOf4} | ${numOf6} | ${runRate}`
+                    );
+                }
+            }
+        }
+    }
+
 }
 
 module.exports = {
